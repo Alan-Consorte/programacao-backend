d20d00d26d14955ae08c21fa57ea4cd852edb44b
Alan-Consorte <a.consorte@escola.pr.gov.br>
2025-05-14 13:53:53 +0000

    Aula06condicionais

---
+++ src/main/java/com/mycompany/Avaliacao01.java
@@ -0,0 +1,12 @@
+package com.mycompany.av
+
+public class Avaliacao02 {
+    public static void main(String[] args) {
+        double areaCirculo = 25; // cm²
+        double piValor = Math.PI; // ou use 3.14
+        double raio = Math.sqrt(areaCirculo / piValor); // r² = A / π => r = sqrt(A / π)
+   
+        // Exibe o resultado
+        System.out.println("O raio do círculo é: " + raio + " cm.");
+    }
+}
---
+++ src/main/java/com/mycompany/Avaliacao04.java
@@ -0,0 +1,14 @@
+package com.mycompany;
+
+public class Avaliacao04 {
+    public static void main(String[] args) {
+        double precoInicial = 850.00;
+        double percentualCorte = 30.0;
+
+        // Calcula o novo preço
+        double precoFinal = precoInicial - (precoInicial * percentualCorte / 100);
+
+        // Exibe o resultado
+        System.out.println("O preço do produto após o desconto é: R$ " + precoFinal);
+    }
+}
